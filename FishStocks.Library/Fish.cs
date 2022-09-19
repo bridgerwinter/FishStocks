e1e7b2a562cacb8862392588fd520108ac520c01
Bridger Winter <bridgerwinter@gmail.com>
2022-09-18 19:59:58 -1000

    Add project files.

---
+++ FishStocks.Library/Fish.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishStocks.Library
+{
+    public class Fish
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ FishStocks.Library/FishTransaction.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishStocks.Library
+{
+    public class FishTransaction
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public Fish Fish { get; set; }
+        public DateTime DateEntered { get; set; }
+
+    }
+}
