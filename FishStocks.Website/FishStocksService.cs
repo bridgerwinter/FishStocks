40cb327c35cc7139b5454c6b325cc8d8be53bee7
Bridger Winter <bridgerwinter@gmail.com>
2022-09-20 22:52:12 -1000

    Database connection work,the connection string contiunes to be null. If this wasn't attached to my professional account I would say how I really feel.

---
+++ FishStocks.Website/FishStocksService.cs
@@ -0,0 +1,18 @@
+﻿using FishStocks.Website.Data.FishStocks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FishStocks.Website
+{
+    public class FishStocksService
+    {
+        private readonly FishStocksContext _context;
+        public FishStocksService(FishStocksContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<FishTransaction>> GetFishTransactionsAsync()
+        {
+            return await _context.FishTransaction.Where(p => p.Id == 1).AsNoTracking().ToListAsync();
+        }
+    }
+}
--- FishStocks.Website/Program.cs
+++ FishStocks.Website/Program.cs
@@ -1,13 +1,16 @@
 using FishStocks.Website;
+using FishStocks.Website.Data.FishStocks;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
+using Microsoft.EntityFrameworkCore;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+ string ConnectionString = @"Data Source=SUNBEAR\SQLEXPRESS;Initial Catalog = FishStocks; Integrated Security = True";
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddAntDesign();
-builder.Services.AddSingleton<DataAccessService>();
-
+//builder.Services.AddAntDesign();
+var x = builder.Configuration.GetSection("iisSettings");
+builder.Services.AddDbContext<FishStocksContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<FishStocksService>();
 await builder.Build().RunAsync();
